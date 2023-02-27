8a4424b82a5ad96066ced3ec71c359fbf08c0d60
LewisF5 <124595419+LewisF5@users.noreply.github.com>
2023-02-27 15:57:56 +0000

    added array

--- Alien-Arch-Unity/Assets/scripts/TestScript.cs
+++ Alien-Arch-Unity/Assets/scripts/TestScript.cs
@@ -8,15 +8,63 @@ public class TestScript : MonoBehaviour
     public string startMessage;
     public string updatemessage;
 
+    public string message1;
+    public string message2;
+    public string message3;
+    public string message4;
+    public string message5;
+
+    public int messagechoice;
+
+    public string[] messages;
+
     // Start is called before the first frame update
+    
     void Start()
     {
-        Debug.Log(startMessage);
-    }
+        /*
+        //Debug.Log(startMessage);//
 
-    // Update is called once per frame
+        //messagechoice = Random.Range(1, 6);//
+        
+        if (messagechoice == 1)
+        {
+            Debug.Log(messages[0]);
+        }
+        else if (messagechoice == 2)
+        {
+            Debug.Log(messages[1]);
+        }
+        else if (messagechoice == 3)
+        {
+            Debug.Log(messages[2]);
+        }
+        
+        else
+        {
+            Debug.Log("invalid message choice");
+        }
+        */
+        if (messagechoice <= messages.Length - 1 && messagechoice >= 0)
+        {
+            Debug.Log(messages[messagechoice]);
+        }
+        else
+        {
+            Debug.Log("invalid message choice");
+        }
+        
+    }
+        
+    // Update is called once per frame//
+
+    
+
     void Update()
     {
         Debug.Log(updatemessage);
     }
 }
+
+    
+
